34e81b1b3c2e0838af9279176c2a2d579e6b6eb4
Vishnu P S <vishnu@evolvier.com>
2019-12-09 11:39:20 +0530

    NetInfo.addEventListener update

--- src/Meteor.js
+++ src/Meteor.js
@@ -89,8 +89,8 @@ module.exports = {
       ...options,
     });
 
-    NetInfo.isConnected.addEventListener('connectionChange', isConnected => {
-      if (isConnected && Data.ddp.autoReconnect) {
+    NetInfo.addEventListener(state => {
+      if (state.isConnected && Data.ddp.autoReconnect) {
         Data.ddp.connect();
       }
     });
